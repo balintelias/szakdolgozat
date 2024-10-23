646b6670fcbbad64af6e20fafbeede0717269dad
Elias Balint <ebalint896@gmail.com>
2024-10-23 13:58:50 +0200

    X2 func should be done

--- sim.py
+++ sim.py
@@ -1,5 +1,6 @@
 # import stuff
 import numpy as np
+import math
 
 
 def r(m):
@@ -17,26 +18,49 @@ def c(n, C_init):
 
 def X1(n):
     # initialise
-    X = np.zeros(n + 31)
+    X = np.zeros(n)
     X[0] = 1
 
     # cyclic shifting
-    for i in range(n - 31):
+    for i in range(n + 31):
         print(f"cyclic shift: {i} {i + 31} / {n}")
         X[i + 31] = (X[i + 3] + X[i]) % 2
 
     return X
 
 
-def X2():
-    C_init = 0
-    for i in range(30):
+def X2(n):
+    X = np.zeros(n)
 
-        return X
+    c_init = C_init(5, 6, 7, 8)  # arbitrary numbers
+    c_init_b = format(c_init, "31b")
+    c_init_b = c_init_b.replace(" ", "0")
+
+    for i in range(31):
+        X[i] = c_init_b[-1 - i]
+
+    for i in range(n - 31):
+        X[i + 31] = (X[i + 3] + X[i]) % 2
+
+    print(f"{n} and {X[n-1]}")
+    return X[n-1] # Solve indexing
+
+
+def C_init(n_PRS_idseq, Nslot_symb, n, l):
+    # n: slot number
+    # l OFDM symbol within the slot
+    # n_PRS_idseq: 0,1,...,4095
+    c_init = (
+        2**22 * math.floor(n_PRS_idseq / 1024)
+        + 2**10 * (Nslot_symb * n + l + 1) * (2 * (n_PRS_idseq % 1024) + 1)
+        + (n_PRS_idseq % 1024)
+    ) % 2**31
+
+    return c_init
+
 
 """
 MAIN STARTING HERE
 """
 
-# Global variable?
-C_init = 5151
+X2(35)
